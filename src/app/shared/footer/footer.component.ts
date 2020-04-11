648f4b5a1701b0c4f7797e934f373c4ccd174855
mahernajar <mahernajar87@gmail.com>
2020-04-12 00:42:56 +0200

    Google Maps API key no longer exposed

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -11,8 +11,7 @@ export class FooterComponent {
   url;
 
   constructor() {
-    this.url = `https://www.google.com/maps/embed/v1/view?key=${environment.embedMapApi}&center=43.294700,5.367967&zoom=16
-    &maptype=satellite`;
+    this.url = `https://www.google.com/maps/embed/v1/view?key=${environment.embedMapApi}&center=43.294700,5.367967&zoom=16&maptype=satellite`;
   }
   onSubmit(f: NgForm) {}
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  embedMapApi: 'AIzaSyCrUdNlIISKGTiRMHXGETYxrgavZmGRu4A',
+  embedMapApi: 'blablabla',
 };
 
 /*
